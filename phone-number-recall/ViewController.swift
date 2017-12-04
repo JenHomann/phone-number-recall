54192ed278386f56f489b9f27e3a7be237c1c28f
Jen Homann <jenhomann@Jens-MBP.igs2.com>
2017-12-04 16:24:38 -0600

    initial commit and basic app functionality

--- phone-number-recall/ViewController.swift
+++ phone-number-recall/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var phoneField: UITextField!
+    @IBOutlet var phoneLabel: UILabel!
+    @IBAction func saveButton(_ sender: Any) {
+        if phoneLabel.text != nil {
+            UserDefaults.standard.set(phoneField.text, forKey: "phone")
+            getPhoneNumber()
+        } else {
+            phoneLabel.text = "You must enter a phone number to save!"
+        }
+    }
+    
+    func getPhoneNumber() {
+        let phoneObject = UserDefaults.standard.object(forKey: "phone")
+        if let phone = phoneObject as? String {
+            phoneLabel.text = "Your saved phone # is \(phone)."
+        } else {
+            phoneLabel.text = "No # stored. Enter one below."
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getPhoneNumber()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
